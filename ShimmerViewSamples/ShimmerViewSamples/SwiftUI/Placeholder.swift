5a0865f416997f4ef4040c44988817d37c3c92eb
hagmas <hgmsk1985@gmail.com>
2022-02-22 15:54:56 +0900

    Refactor

--- ShimmerViewSamples/ShimmerViewSamples/SwiftUI/Placeholder.swift
+++ ShimmerViewSamples/ShimmerViewSamples/SwiftUI/Placeholder.swift
@@ -12,13 +12,10 @@ public struct Placeholder: View {
             LazyVStack(alignment: .leading, spacing: 0) {
                 Spacer(minLength: 16)
                 ShimmerElement(width: 100, height: 12)
-                    .padding(.leading, 16)
                 Spacer(minLength: 16)
                 ShimmerElement(height: 60)
-                    .padding(.horizontal, 16)
                 Spacer(minLength: 24)
                 ShimmerElement(width: 100, height: 12)
-                    .padding(.leading, 16)
                 Spacer(minLength: 16)
                 ForEach(0..<16) { _ in
                     HStack(alignment: .center, spacing: 4) {
@@ -32,10 +29,10 @@ public struct Placeholder: View {
                             .aspectRatio(1, contentMode: .fit)
                             .cornerRadius(4)
                     }
-                    .padding(.horizontal, 16)
                     .padding(.bottom, 4)
                 }
             }
+            .padding(.horizontal, 16)
         }
     }
 
