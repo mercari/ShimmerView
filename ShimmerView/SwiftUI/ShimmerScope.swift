8f9c63517373e59b3b131d52d6213fa524f12460
hagmas <hgmsk1985@gmail.com>
2022-02-24 16:34:34 +0900

    Refactor ShimmerScope

--- ShimmerView/SwiftUI/ShimmerScope.swift
+++ ShimmerView/SwiftUI/ShimmerScope.swift
@@ -2,8 +2,9 @@ import SwiftUI
 
 @available(iOS 14.0, *)
 public struct ShimmerScope<Content: View>: View {
-    @StateObject private var model: ShimmerState
+    @StateObject private var state = ShimmerState()
     @Binding private var isAnimating: Bool
+    private let style: ShimmerViewStyle
     private let content: () -> Content
 
     public init(
@@ -11,7 +12,7 @@ public struct ShimmerScope<Content: View>: View {
         isAnimating: Binding<Bool>,
         content: @escaping () -> Content
     ) {
-        _model = StateObject(wrappedValue: ShimmerState(style: style, isAnimating: isAnimating.wrappedValue))
+        self.style = style
         _isAnimating = isAnimating
         self.content = content
     }
@@ -20,30 +21,26 @@ public struct ShimmerScope<Content: View>: View {
         GeometryReader { proxy in
             content()
                 .environment(\.shimmerGeometry, proxy)
-                .environmentObject(model)
+                .environmentObject(state)
+        }
+        .onAppear {
+            state.style = style
+            state.setAnimating(isAnimating)
         }
         .onChange(of: isAnimating) { isAnimating in
-            model.setAnimating(isAnimating)
+            state.setAnimating(isAnimating)
         }
     }
 }
 
 @available(iOS 14.0, *)
 internal class ShimmerState: ObservableObject {
-    let style: ShimmerViewStyle
+    @Published var style: ShimmerViewStyle = .default
     @Published var isAnimating: Bool = false
     @Published var effectBeginTime: CFTimeInterval = 0
-    
-    init(style: ShimmerViewStyle, isAnimating: Bool) {
-        self.style = style
-        if isAnimating {
-            self.isAnimating = isAnimating
-            effectBeginTime = CACurrentMediaTime()
-        }
-    }
-    
+
     func setAnimating(_ isAnimating: Bool) {
-        if isAnimating {
+        if self.isAnimating != isAnimating && isAnimating {
             effectBeginTime = CACurrentMediaTime()
         }
         self.isAnimating = isAnimating
